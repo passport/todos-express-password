125e166444f0658ce08cc5e1db2c57f9a9bd92ac
Jared Hanson <jaredhanson@gmail.com>
2015-08-20 14:16:24 -0700

    Set resave and saveUninitialized to false.

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.set('view engine', 'ejs');
 app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 
 // Initialize Passport and restore authentication state, if any, from the
 // session.
