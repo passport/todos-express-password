79716cdf18e9892fb75acb7c8350441c2d93668c
Rich <piemonkey@gmail.com>
2015-10-10 15:02:04 +0100

    Tweaked login redirect to go to requested page

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.get('/login',
 app.post('/login', 
   passport.authenticate('local', { failureRedirect: '/login' }),
   function(req, res) {
-    res.redirect('/');
+    res.redirect(req.session.returnTo || '/');
   });
   
 app.get('/logout',
