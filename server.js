37589d0da30e29884f83176ebe5537e6df8ba6d1
Behind The Math <info@behindthemath.io>
2016-09-24 21:56:46 -0400

    express-session no longer needs cookie-parser middleware "Since version 1.5.0, the cookie-parser middleware no longer needs to be used for this module to work. This module now directly reads and writes cookies on req/res. Using cookie-parser may result in issues if the secret is not the same between this module and cookie-parser." (https://github.com/expressjs/session/blob/master/README.md)

--- server.js
+++ server.js
@@ -52,7 +52,6 @@ app.set('view engine', 'ejs');
 // Use application-level middleware for common functionality, including
 // logging, parsing, and session handling.
 app.use(require('morgan')('combined'));
-app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 
