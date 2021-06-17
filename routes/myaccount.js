261f8c73e1b7ca47ab26d89137af04d2042ca4f1
Jared Hanson <jaredhanson@gmail.com>
2021-06-17 09:50:49 -0700

    Clean up myaccount route.

--- routes/myaccount.js
+++ routes/myaccount.js
@@ -5,19 +5,21 @@ var db = require('../db');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', ensureLoggedIn(), function(req, res, next) {
-  db.get('SELECT rowid AS id, username, name FROM users WHERE rowid = ?', [ req.user.id ], function(err, row) {
-    if (err) { return next(err); }
+router.get('/',
+  ensureLoggedIn(),
+  function(req, res, next) {
+    db.get('SELECT rowid AS id, username, name FROM users WHERE rowid = ?', [ req.user.id ], function(err, row) {
+      if (err) { return next(err); }
     
-    // TODO: Handle undefined row.
+      // TODO: Handle undefined row.
     
-    var user = {
-      id: row.id.toString(),
-      username: row.username,
-      displayName: row.name
-    };
-    res.render('profile', { user: user });
+      var user = {
+        id: row.id.toString(),
+        username: row.username,
+        displayName: row.name
+      };
+      res.render('profile', { user: user });
+    });
   });
-});
 
 module.exports = router;
