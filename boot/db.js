e5905325f1fecc89542757d9fe303f05aa7f7d42
Jared Hanson <jaredhanson@gmail.com>
2021-06-17 14:55:38 -0700

    Remove dead db code.

--- boot/db.js
+++ boot/db.js
@@ -5,18 +5,6 @@ module.exports = function() {
 
   db.serialize(function() {
     db.run("CREATE TABLE IF NOT EXISTS users (username TEXT, hashed_password BLOB, salt BLOB, name TEXT)");
-
-    /*
-    var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-    for (var i = 0; i < 10; i++) {
-        stmt.run("Ipsum " + i);
-    }
-    stmt.finalize();
-
-    db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-        console.log(row.id + ": " + row.info);
-    });
-    */
   });
 
   //db.close();
