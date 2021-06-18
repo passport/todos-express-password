9a3e590c84e25fb17bacda696efe9d89509da12d
Jared Hanson <jaredhanson@gmail.com>
2021-06-18 08:42:43 -0700

    Add unique constraint on username.

--- boot/db.js
+++ boot/db.js
@@ -4,7 +4,12 @@ var db = require('../db');
 module.exports = function() {
 
   db.serialize(function() {
-    db.run("CREATE TABLE IF NOT EXISTS users (username TEXT, hashed_password BLOB, salt BLOB, name TEXT)");
+    db.run("CREATE TABLE IF NOT EXISTS users ( \
+      username TEXT UNIQUE, \
+      hashed_password BLOB, \
+      salt BLOB, \
+      name TEXT \
+    )");
   });
 
   //db.close();
