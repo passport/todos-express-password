e0f904246a8495b625f873af73242e7c9d4ed312
Jared Hanson <jaredhanson@gmail.com>
2021-06-22 14:42:52 -0700

    Export db with module.exports.

---
+++ db.js
@@ -0,0 +1,3 @@
+var sqlite3 = require('sqlite3');
+
+module.exports = new sqlite3.Database('db.sqlite3');
--- db/index.js
+++
@@ -1,3 +0,0 @@
-var sqlite3 = require('sqlite3');
-
-exports = module.exports = new sqlite3.Database('db.sqlite3');
