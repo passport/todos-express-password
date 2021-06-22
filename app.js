a5abf87a42f1ede90cd3c1e27e681b8b94fedbe1
Jared Hanson <jaredhanson@gmail.com>
2021-06-22 14:41:16 -0700

    Add cookie-parser.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var passport = require('passport');
 var path = require('path');
+var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -22,9 +23,8 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 app.use(passport.initialize());
 app.use(passport.session());
