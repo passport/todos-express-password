ab7882b22d08b330dbdd70c01e3c73f22ca5b245
Jared Hanson <jaredhanson@gmail.com>
2022-01-10 15:44:35 -0800

    Copy stylesheets to public directory.

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname))); // TODO: Improve this
 app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 app.use(function(req, res, next) {
   var msgs = req.session.messages || [];
