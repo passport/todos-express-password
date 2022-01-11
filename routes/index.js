32f8c142058154a4393700becb99e4bd47fd32de
Jared Hanson <jaredhanson@gmail.com>
2022-01-10 16:17:46 -0800

    Preserve filter on clear completed.

--- routes/index.js
+++ routes/index.js
@@ -114,13 +114,13 @@ router.post('/toggle-all', ensureLoggedIn(), function(req, res, next) {
   });
 });
 
-router.post('/clear', ensureLoggedIn(), function(req, res, next) {
+router.post('/clear-completed', ensureLoggedIn(), function(req, res, next) {
   db.run('DELETE FROM todos WHERE owner_id = ? AND completed = ?', [
     req.user.id,
     1
   ], function(err) {
     if (err) { return next(err); }
-    return res.redirect('/');
+    return res.redirect('/' + (req.body.filter || ''));
   });
 });
 
