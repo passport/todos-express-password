c1d18e6ac6e69c86ef6659e6aa52b040877fdceb
Jared Hanson <jaredhanson@gmail.com>
2022-01-13 18:05:39 -0800

    Consolidate db code into a single file.

--- boot/db.js
+++ boot/db.js
@@ -3,20 +3,7 @@ var db = require('../db');
 
 module.exports = function() {
 
-  db.serialize(function() {
-    db.run("CREATE TABLE IF NOT EXISTS users ( \
-      username TEXT UNIQUE, \
-      hashed_password BLOB, \
-      salt BLOB, \
-      name TEXT \
-    )");
-    
-    db.run("CREATE TABLE IF NOT EXISTS todos ( \
-      owner_id INTEGER NOT NULL, \
-      title TEXT NOT NULL, \
-      completed INTEGER \
-    )");
-  });
+  
 
   //db.close();
 
--- db.js
+++ db.js
@@ -1,3 +1,20 @@
 var sqlite3 = require('sqlite3');
 
-module.exports = new sqlite3.Database('db.sqlite3');
+var db = new sqlite3.Database('db.sqlite3');
+
+db.serialize(function() {
+  db.run("CREATE TABLE IF NOT EXISTS users ( \
+    username TEXT UNIQUE, \
+    hashed_password BLOB, \
+    salt BLOB, \
+    name TEXT \
+  )");
+  
+  db.run("CREATE TABLE IF NOT EXISTS todos ( \
+    owner_id INTEGER NOT NULL, \
+    title TEXT NOT NULL, \
+    completed INTEGER \
+  )");
+});
+
+module.exports = db;
