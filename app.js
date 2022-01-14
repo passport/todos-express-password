19625ace05edf2c812463e7336d2ca7d95a65196
Jared Hanson <jaredhanson@gmail.com>
2022-01-13 21:14:07 -0800

    Remove scripts from views, update app.js to match generator.

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+var createError = require('http-errors');
 var express = require('express');
-var passport = require('passport');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var passport = require('passport');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -11,12 +12,13 @@ var app = express();
 
 app.locals.pluralize = require('pluralize');
 
+// WIP: messages
+//WIP: sqlite persisted session
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Use application-level middleware for common functionality, including
-// logging, parsing, and session handling.
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -32,8 +34,23 @@ app.use(function(req, res, next) {
 });
 app.use(passport.authenticate('session'));
 
-// Define routes.
 app.use('/', indexRouter);
 app.use('/', authRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
